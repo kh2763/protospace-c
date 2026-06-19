518271a095d44172e08513f8ef8e76cd1a850a7d
yoshioka1 <yoshioka.shunpei@minebea-ss.com>
2026-06-19 14:49:53 +0900

    UserEntityの作成

--- src/main/java/in/tech_camp/protospace_c/entity/UserEntity.java
+++ src/main/java/in/tech_camp/protospace_c/entity/UserEntity.java
@@ -1,5 +1,13 @@
 package in.tech_camp.protospace_c.entity;
 
+import lombok.Data;
+
+@Data
 public class UserEntity {
-  
+  private Integer id;
+  private String email;
+  private String password;
+  private String user_name;
+  private String team;
+  private String job_title;
 }
