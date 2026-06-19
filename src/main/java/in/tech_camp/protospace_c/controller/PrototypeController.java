41e30ecf2c93ff67eedbe01b42ee9c02cf25e934
ikenaga313 <ikenaga.aoi@minebea-ss.com>
2026-06-19 15:25:53 +0900

    @Autowiredとimport文

--- src/main/java/in/tech_camp/protospace_c/controller/PrototypeController.java
+++ src/main/java/in/tech_camp/protospace_c/controller/PrototypeController.java
@@ -1,9 +1,14 @@
 package in.tech_camp.protospace_c.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import in.tech_camp.protospace_c.repository.PrototypeRepository;
 @Controller
 public class PrototypeController {
+
+@Autowired
+private PrototypeRepository prototypeRepository;
 
 }
