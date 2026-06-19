3a3dfd1457682c30bf32e8ffc3dbdf960ee213e7
ikenaga313 <ikenaga.aoi@minebea-ss.com>
2026-06-19 17:09:15 +0900

    Prototype Repositoryをインターフェースに取得し、findAllメソッドを追加

--- src/main/java/in/tech_camp/protospace_c/repository/PrototypeRepository.java
+++ src/main/java/in/tech_camp/protospace_c/repository/PrototypeRepository.java
@@ -1,5 +1,17 @@
 package in.tech_camp.protospace_c.repository;
 
-public class PrototypeRepository {
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+import in.tech_camp.protospace_c.entity.PrototypeEntity;
+@Mapper
+public interface PrototypeRepository {
+  
+  @Select("SELECT * FROM prototypes")
+  List<PrototypeEntity> findAll();
+
   
 }
