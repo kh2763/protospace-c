2c4306e2738282497660fee7d1e93f154a02df4f
yoshioka1 <yoshioka.shunpei@minebea-ss.com>
2026-06-19 15:06:31 +0900

    データベースに新規ユーザーを登録する

--- src/main/java/in/tech_camp/protospace_c/repository/UserRepository.java
+++ src/main/java/in/tech_camp/protospace_c/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package in.tech_camp.protospace_c.repository;
 
-public class UserRepository {
-  
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.protospace_c.entity.UserEntity;
+
+@Mapper
+public interface UserRepository {
+  // フォームの内容を受け取ってデータベースに登録する
+  @Insert("INSERT INTO users (email, password, user_name, team, job_tylte) VALUES (#{email}, #{password}, #{user_name}, #{team}, #{job_tytle})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(UserEntity userEntity);
 }
