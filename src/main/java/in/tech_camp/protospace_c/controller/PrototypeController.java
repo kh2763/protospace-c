adf6935b85556a3495e07e214b34a2681eb07911
MSSYK <yanagishita.kouki@minebea-ss.com>
2026-06-19 15:41:01 +0900

    投稿ページ移動のメソッド作成

--- src/main/java/in/tech_camp/protospace_c/controller/PrototypeController.java
+++ src/main/java/in/tech_camp/protospace_c/controller/PrototypeController.java
@@ -1,5 +1,23 @@
 package in.tech_camp.protospace_c.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.tech_camp.protospace_c.form.PrototypeForm;
+import lombok.AllArgsConstructor;
+
+
+@Controller
+@AllArgsConstructor
 public class PrototypeController {
-  
+
+  //投稿ページ移動メソッド
+  @GetMapping("/prototypes/new")
+  public String showPrototypeNew(Model model){
+    model.addAttribute("prototypeForm", new PrototypeForm());
+    return "prototypes/new";
+  }
+
+
 }
