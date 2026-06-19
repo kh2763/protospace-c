0e6f3608495942519eec216ab34253f60a525a6c
yoshioka1 <yoshioka.shunpei@minebea-ss.com>
2026-06-19 15:50:28 +0900

    UserFormの作成

--- src/main/java/in/tech_camp/protospace_c/form/UserForm.java
+++ src/main/java/in/tech_camp/protospace_c/form/UserForm.java
@@ -1,5 +1,35 @@
 package in.tech_camp.protospace_c.form;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.BindingResult;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserForm {
+  @NotBlank(message = "Email can't be blank")
+  @Email(message = "Email should be valid")
+  private String email;
   
+  @NotBlank(message = "Password can't be blank")
+  @Length(min = 6, max = 128, message = "Password should be between 6 and 128 characters")
+  private String password;
+
+  private String passwordConfirmation;
+
+  //パスワードの正誤確認についてのメソッド
+  public void validatePasswordConfirmation(BindingResult result) {
+      if (!password.equals(passwordConfirmation)) {
+          result.rejectValue("passwordConfirmation", "passwordconfirmation_error", "Password confirmation doesn't match Password");
+      }
+  }
+
+  @NotBlank(message = "Username can't be blank")
+  private String user_name;
+
+  @NotBlank(message = "Team can't be blank")
+  private String team;
+
+  @NotBlank(message = "Job tytle can't be blank")
+  private String job_tytle;
 }
