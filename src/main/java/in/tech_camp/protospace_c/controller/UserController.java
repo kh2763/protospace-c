2cfdfb688e16fcfa89452addbd29f32657c42a28
yoshioka1 <yoshioka.shunpei@minebea-ss.com>
2026-06-19 16:22:54 +0900

    ユーザー登録ページの表示

--- src/main/java/in/tech_camp/protospace_c/controller/UserController.java
+++ src/main/java/in/tech_camp/protospace_c/controller/UserController.java
@@ -1,5 +1,21 @@
 package in.tech_camp.protospace_c.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import in.tech_camp.protospace_c.form.UserForm;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
 public class UserController {
+  
+
+  // ユーザー登録ページの表示
+  @GetMapping("/users/sign_up")
+  public String showSignUp(Model model){
+    model.addAttribute("userForm", new UserForm());
+    return "users/signUp";
+  }
   
 }
